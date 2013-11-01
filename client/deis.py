f7a760c952951a2934a5f1b0d52aa709f27b5a69
Tom Buckley-Houston <tom@tombh.co.uk>
2013-11-01 18:38:26 +0000

    Full Vagrant provider. Squashed commits

--- client/deis.py
+++ client/deis.py
@@ -399,11 +399,11 @@ class DeisClient(object):
             progress.join()
         if response.status_code == requests.codes.created:  # @UndefinedVariable
             data = response.json()
-            app_id = data['id']
-            print("done, created {}".format(app_id))
+            formation = data['id']
+            print("done, created {}".format(formation))
             # add a git remote
             hostname = urlparse.urlparse(self._settings['controller']).netloc
-            git_remote = "git@{hostname}:{app_id}.git".format(**locals())
+            git_remote = "git@{hostname}:{formation}.git".format(**locals())
             try:
                 subprocess.check_call(
                     ['git', 'remote', 'add', '-f', 'deis', git_remote],
@@ -508,6 +508,7 @@ class DeisClient(object):
                                   "/api/apps/{}/calculate".format(app))
         if response.status_code == requests.codes.ok:  # @UndefinedVariable
             databag = json.loads(response.content)
+            print(databag)
             domains = databag.get('domains', [])
             if domains:
                 domain = random.choice(domains)
@@ -1634,7 +1635,6 @@ class DeisClient(object):
 
         Usage: deis providers:create <id> <type> <creds>
         """
-        # TODO: This function has a McCabe of 13. Refactor for simplicity.
         type = args.get('<type>')  # @ReservedAssignment
         if type == 'ec2':
             # read creds from envvars
@@ -1731,15 +1731,13 @@ class DeisClient(object):
         else:
             print('No Rackspace credentials discovered.')
         if 'DIGITALOCEAN_API_KEY' in os.environ and 'DIGITALOCEAN_CLIENT_ID' in os.environ:
-            print("Found Digitalocean credentials: {}".format(
-                os.environ['DIGITALOCEAN_CLIENT_ID']))
+            print("Found Digitalocean credentials: {}".format(os.environ['DIGITALOCEAN_CLIENT_ID']))
             inp = raw_input('Import these credentials? (y/n) : ')
             if inp.lower().strip('\n') != 'y':
                 print('Aborting.')
             else:
-                creds = {
-                    'client_id': os.environ['DIGITALOCEAN_CLIENT_ID'],
-                    'api_key': os.environ['DIGITALOCEAN_API_KEY'],
+                creds = {'client_id': os.environ['DIGITALOCEAN_CLIENT_ID'],
+                         'api_key': os.environ['DIGITALOCEAN_API_KEY'],
                 }
                 body = {'creds': json.dumps(creds)}
                 sys.stdout.write('Uploading Digitalocean credentials... ')
@@ -1752,6 +1750,51 @@ class DeisClient(object):
                     raise ResponseError(response)
         else:
             print('No Digitalocean credentials discovered.')
+
+        # Check for locally booted Deis Controller VM
+        try:
+            running_vms = subprocess.check_output(
+                    ['vboxmanage', 'list', 'runningvms'],
+                    stderr=subprocess.PIPE)
+        except subprocess.CalledProcessError:
+            running_vms = ""
+        # Vagrant internally names a running VM using the folder name in which the Vagrantfile resides, eg;
+        # my-deis-code-folder_default_1383326629
+        deis_codebase_folder = self._session.git_root().split('/')[-1]
+        if deis_codebase_folder in running_vms:
+            print("Detected locally running Deis Controller VM")
+            # In order for the Controller to be able to boot Vagrant VMs it needs to run commands on the host machine.
+            # It does this via an SSH server. In order to access that server we need to send the current user's name
+            # and IP address.
+            try:
+                user = subprocess.check_output(
+                    "whoami",
+                    stderr=subprocess.PIPE
+                ).strip()
+                ip = subprocess.check_output(
+                    "/sbin/ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | head -n1",
+                    stderr=subprocess.PIPE,
+                    shell=True
+                ).strip()
+            except subprocess.CalledProcessError:
+                print("Error detecting username and host IP address.")
+                sys.exit(1)
+            creds = {
+                'user': user,
+                'ip': ip
+            }
+            body = {'creds': json.dumps(creds)}
+            sys.stdout.write('Activating Vagrant as a provider... ')
+            sys.stdout.flush()
+            response = self._dispatch('patch', '/api/providers/vagrant',
+                                      json.dumps(body))
+            if response.status_code == requests.codes.ok:  # @UndefinedVariable
+                print('done')
+            else:
+                raise ResponseError(response)
+        else:
+            print("No Vagrant VMs detected")
+
 
     def providers_info(self, args):
         """
